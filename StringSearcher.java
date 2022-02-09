7fcebf91da52e2c81ad358299ee6522899302833
Shreyas Casturi <shreyascasturi@gmail.com>
2022-02-09 10:15:39 -0500

    some problems in shifting

--- StringSearcher.java
+++ StringSearcher.java
@@ -1,25 +1,30 @@
 import java.util.HashMap;
 
-
+// a very basic implementation of Boyer-Moore with only bad-character rule.
 public class StringSearcher {
-    private static HashMap<Character, Integer> pat_table;
+
+    private static HashMap<Character, Integer> pat_map;
     private static String pattern;
     private static String sequence;
+
     public static void main(String[] args) {
         if (args.length != 2) {
             System.out.println("GREP will not work. Terminating.");
             
         } else {
             pattern = args[0];
-            sequence = args[1]
-            pat_table = StringSearcher.pprocess(args[0]);
-            int pat_num_occurrences = StringSearcher.reGREP(args[0], args[1]);
-            System.out.printf("the number of occurrences of %s in %s is %d. Terminating.\n", args[0], args[1], pat_num_occurrences);
+            sequence = args[1];
+            pat_map = StringSearcher.pre_process(args[0]);
+            int pat_num_occurrences = StringSearcher.reGREP(pattern, sequence);
+            System.out.printf("the number of occurrences of %s in %s is %d. Terminating.\n", pattern, sequence, pat_num_occurrences);
         }
     }
 
     // implementation of boyer moore string searching algorithm (basic)
     public static int reGREP(String pattern, String sequence) {
+
+        System.out.printf("pattern is: %s\n", pattern);
+        System.out.printf("sequence is: %s\n", sequence);
         
         int num_occurrences = 0;
 
@@ -42,26 +47,28 @@ public class StringSearcher {
                 
                 char pat_char = pattern.charAt(pat_iter_idx);
                 char seq_char = sequence.charAt(pat_iter_idx);
+
                 // check each char.
-                // if equal, decrement.
-                // else, shift completely or partial shift.
+                // if equal, continue
+                // else, do shift
                 if (pat_char == seq_char) {
                     pat_iter_idx--;
                 } else if (pat_map.keySet().contains(seq_char)) {
-                    // we know pattern contains seq char somewhere.
-                    // we need to determine if last occurrence of pat char is BEFORE seq char.
-                    // if it is, then we shift up by that amount of space/modify string
-                    
-                    int shift_up_amt = StringSearcher.next_shift(pat_map, pat_ider_idx);
+                    // do bad char shift if possible. 
+                    int shift_up_amt = StringSearcher.bad_char_shift(pat_map, pat_iter_idx, pat_length);
                     seq_length -= shift_up_amt;
+                    System.out.printf("new substring after partial bad char shift: %s\n", sequence);
                     break;
-                    
                 } else {
-                    //String seq_rem_string = sequence.substring(pat_length + 1);
-                    //sequence = seq_rem_string;
-                    int shift_up_amt = 
+                    // do whole shift
+                    int shift_up_amt = pat_length;
+                    seq_length -= shift_up_amt;
+                    sequence = sequence.substring(shift_up_amt);
+                    System.out.printf("new substring after whole shift: %s\n", sequence);
+                    break;
                 }
             }
+            
             if (seq_length < pat_length) {
                 return num_occurrences;
             } 
@@ -71,7 +78,7 @@ public class StringSearcher {
             
     }
 
-    public static HashMap<Character, Integer> pprocess(String pattern) {
+    public static HashMap<Character, Integer> pre_process(String pattern) {
         HashMap<Character, Integer> pat_map = new HashMap<>();
         for (int i = 0; i < pattern.length(); i++) {
             pat_map.put(pattern.charAt(i), i);
@@ -87,7 +94,7 @@ public class StringSearcher {
     // seq: A at loc 2 (pat iter idx)
     // pat: A at loc 0 (seq char pat idx)
     // move!
-    public static int find_shift(HashMap<Character, Integer> pat_map, int pat_iter_idx) {
+    public static int bad_char_shift(HashMap<Character, Integer> pat_map, int pat_iter_idx, int pat_length) {
         int seq_char_pat_idx = pat_map.get(StringSearcher.sequence.charAt(pat_iter_idx));
 
         if (seq_char_pat_idx < pat_iter_idx ) {
@@ -95,8 +102,10 @@ public class StringSearcher {
             int shift_up_amt = pat_iter_idx - seq_char_pat_idx;
             StringSearcher.sequence = StringSearcher.sequence.substring(shift_up_amt);
             return shift_up_amt;
-        } else { // calculate regular shift
-            
+        } else { // shift by reg amt 
+            int shift_up_amt = 1;
+            StringSearcher.sequence = StringSearcher.sequence.substring(shift_up_amt);
+            return shift_up_amt;
         }
     }
 }
